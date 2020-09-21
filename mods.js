954c64dadd8faf86ca5f383ea8407174e9fcac88
dcorso21 <david.l.corson@gmail.com>
2020-09-21 19:23:37 -0400

    js course coding challenge 7

---
+++ mods.js
@@ -0,0 +1,16 @@
+
+class CALC {
+    constructor() {
+        this.description = "Calculation Functions";
+    }
+
+    add(num_a, num_b) {
+        return num_a + num_b;
+    }
+    subtract(num_a, num_b) {
+        return num_a - num_b;
+    }
+    divide(num_a, num_b) {
+        return num_a / num_b;
+    }
+}
--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-class CALC {
-    constructor() {
-        this.description = "Calculation Functions";
-    }
+var questions = {},
+    currentScore = 0,
+    gameOn = true;
 
-    add(num_a, num_b) {
-        return num_a + num_b;
+var currentQuestion, lastQuestion;
+
+
+function addQuestion(text, options, ansInd) {
+    questions[text] = {
+        options: options,
+        answer: ansInd,
+    };
+}
+
+addQuestion("What is your last name?", ["Corson", "Biagioni", "Carsion"], 0);
+addQuestion("What is your first name?", ["Jeremiah", "David", "James"], 1);
+
+function pickQuestion() {
+    let questionText = Object.keys(questions);
+    let selection = ''
+    do {
+        selection = Math.floor(Math.random()*questionText.length)
+        selection = questionText[selection]
+    } while (String(selection) === lastQuestion);
+    // if (String(questionText[selection]) == lastQuestion) {pickQuestion()}
+    lastQuestion = String(selection);
+    currentQuestion = selection;
+}
+
+function AskQuestion() {
+    let ops = questions[currentQuestion].options;
+    let msg = "";
+    for (let i = 0; i < ops.length; i++) {
+        msg += `${i + 1}. ${ops[i]}\n`;
     }
-    subtract(num_a, num_b) {
-        return num_a - num_b;
-    }
-    divide(num_a, num_b) {
-        return num_a / num_b;
+    console.log(msg);
+    let ans = prompt(currentQuestion);
+    gameOn = ans === null ? false : true;
+    let correctIndex = questions[currentQuestion].answer;
+    if (correctIndex === ans - 1) {
+        console.log("Correct!");
+        currentScore +=1
+        console.log(`Current Score: ${currentScore}`);
+    } else {
+        gameOn ? console.log("Nopers"): null;
     }
 }
 
-const Calc = new CALC();
+function init() {
+    while (gameOn) {
+        pickQuestion();
+        AskQuestion();
+    }
+    console.log(`Game Complete! Final Score = ${currentScore}`);
+}
 
-let s = Calc.divide(1, 2);
-console.log(s);
-// console.log(s);
+init()
+
