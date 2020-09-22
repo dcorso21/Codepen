7090d216b361606241d615d4d900972833f4a6c9
dcorso21 <david.l.corson@gmail.com>
2020-09-22 00:59:11 -0400

    docstrings complete

--- script.js
+++ script.js
@@ -1,75 +1,120 @@
-var currentQuestion;
+var parks = {},
+    streets = {};
 
-var questions = {},
-    currentScore = 0,
-    gameOn = true,
-    lastQuestion = [];
-
-function addQuestion(text, options, ansInd) {
-    questions[text] = {
-        options: options,
-        answer: ansInd,
+/**
+ * ## Add Location
+ * @param {string} type -> 'park' or 'street'
+ * @param {string} name -> name of park or street
+ * @param {number} buildYear -> year street or park was founded
+ * @param {number} numTrees -> number of trees
+ * @param {number} area -> area in sq miles
+ */
+function addLocation(type, name, buildYear, numTrees, area) {
+    let info = {
+        buildYear: buildYear,
+        numTrees: numTrees,
+        area: area,
+        density: numTrees / area,
+        age: 2020 - buildYear,
     };
+    type === "park" ? (parks[name] = info) : (streets[name] = info);
 }
 
-// Set Questions 
-addQuestion("What is your last name?", ["Corson", "Biagioni", "Carsion"], 0);
-addQuestion("What is your first name?", ["Jeremiah", "David", "James"], 1);
-addQuestion("What is your age", ["25", "56", "109"], 0);
-addQuestion("Fav Color?", ["Green", "Blue", "Red"], 2);
-addQuestion("How cute is Bailey?", ["Very", "Extremely", "So Cute!!!"], 2);
-
-function pickQuestion() {
-    let questionText = Object.keys(questions);
-    // If all questions have been asked, End the game
-    if (questionText.length <= lastQuestion.length) {
-        gameOn = false;
-        return;
+/**
+ * ## Printer Functions in one module for easy logging
+ */
+class Printer {
+    constructor() {
+        this.description = "handles console logging";
     }
-    let selection;
-    // Ensure that the question has not been asked before
-    do {
-        selection = Math.floor(Math.random() * questionText.length);
-        selection = questionText[selection];
-    } while (lastQuestion.includes(String(selection)));
-    currentQuestion = selection;
+    /**
+     * ## Describe All Streets
+     */
+    descAllStreets() {
+        Object.keys(streets).map((streetname)=>{
+            let street = streets[streetname]
+            if (street.area <= 500) {
+                street.size = "large";
+            } else if (street.area <= 1000) {
+                street.size = "medium";
+            } else if (street.area <= 1500) {
+                street.size = "large";
+            } else {
+                street.size = "giant";
+            }
+            console.log(
+                `${streetname} built in ${street.buildYear} is a ${street.size} street`
+            );
+        })
+    }
+
+    /**
+     * ## Describe All Parks - logs all parks
+     */
+    descAllParks() {
+        Object.keys(parks).map((park) => {
+            console.log(
+                `${park} has a tree density of ${parks[park].density} trees / sq mile`
+            );
+        });
+    }
+    /**
+     * ## Logs densest park with info
+     */
+    densestPark() {
+        let parkNames = Object.keys(parks);
+        let densest = parkNames.reduce((a, b) => {
+            return parks[a].density > parks[b].density ? a : b;
+        });
+        // },  parkNames[0]);
+        console.log(
+            `${densest} is the most dense with a density of ${parks[densest].density} trees / sq mile`
+        );
+    }
+    /**
+     * ## Logs all parks with density over 1000
+     */
+    denseOverThousand() {
+        let parkNames = Object.keys(parks);
+        let overThousand = parkNames.reduce((acc, name) => {
+            parks[name].numTrees >= 1000 ? acc.push(name) : null;
+            return acc;
+        }, []);
+        overThousand.map((name) => console.log(`${name} has over 1000 trees`));
+    }
+    /**
+     * ## Logs the average age of all streets
+     */
+    avgStreetAge() {
+        let avg =
+            Object.keys(streets).reduce((a, b) => a + streets[b].age, 0) /
+            Number(Object.keys(streets).length);
+        console.log(`The average age of the streets is ${avg}`);
+    }
 }
 
-function AskQuestion() {
-    // If the game is over, Dont ask a question
-    if (!gameOn) {return}
-    let ops = questions[currentQuestion].options;
-    let msg = "";
-
-    // Display Options
-    for (let i = 0; i < ops.length; i++) {
-        msg += `${i + 1}. ${ops[i]}\n`;
-    }
-    console.log(msg);
-
-    // Prompt for Answer
-    let ans = prompt(currentQuestion);
-    gameOn = ans === null ? false : true;
-    let correctIndex = questions[currentQuestion].answer;
-
-    // Eval whether True or False
-    if (correctIndex === ans - 1) {
-        console.log("Correct!");
-        currentScore += 1;
-        console.log(`Current Score: ${currentScore}`);
-    } else {
-        gameOn ? console.log("Nopers") : null;
-    }
-}
-
+/**
+ * ## Initializes Reports
+ */
 function init() {
-    while (gameOn) {
-        pickQuestion();
-        AskQuestion();
-        lastQuestion.push(String(currentQuestion));
-        console.log(lastQuestion.length);
-    }
-    console.log(`Game Complete! Final Score = ${currentScore}`);
-}
+    console.log("\n");
+    addLocation("park", "National Park", 1990, 1200, 300);
+    addLocation("park", "Greenboro Park", 1991, 1200, 300);
+    addLocation("park", "Salsbing Park", 1991, 1200, 300);
+    addLocation("street", "Oglethorpe Ave", 1975, 1100, 2200);
+    addLocation("street", "Main Street", 1985, 1150, 2900);
+    addLocation("street", "Fleet Street", 1985, 1150, 2900);
 
+    console.log("----Park Report----");
+    px.descAllParks();
+    px.densestPark();
+    px.denseOverThousand();
+
+    console.log("\n----Street Report----");
+    px.avgStreetAge();
+    px.descAllStreets();
+
+    console.log("\n");
+}
+const px = new Printer();
 init();
